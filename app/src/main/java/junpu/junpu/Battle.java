c2700ab52fd473d9a8562a3f951258bdc9248d8e
tsook <kimts0830@gmail.com>
2018-11-19 17:08:48 +0900

    Implement battle firing and initial health bar and energy bar depletion animations

--- app/src/main/java/junpu/junpu/Battle.java
+++ app/src/main/java/junpu/junpu/Battle.java
@@ -1,10 +1,18 @@
 package junpu.junpu;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,9 +20,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -25,10 +35,40 @@ public class Battle extends AppCompatActivity {
     JSONObject userData;
     JSONObject battleData;
 
+    TextView oppUsername;
+    TextView oppLevel;
+    TextView oppHealthbar;
+    ImageView oppPrimary;
+    ImageView oppSecondary;
+    ImageView oppTertiary;
+
+    TextView userUsername;
+    TextView userLevel;
+    TextView userHealthbar;
+    TextView userEnergybar;
+    ImageView userPrimary;
+    ImageView userSecondary;
+    ImageView userTertiary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+
+        oppUsername = (TextView) findViewById(R.id.opponent_username);
+        oppLevel = (TextView) findViewById(R.id.opponent_level);
+        oppHealthbar = (TextView) findViewById(R.id.opponent_healthbar);
+        oppPrimary = (ImageView) findViewById(R.id.opp_primary);
+        oppSecondary = (ImageView) findViewById(R.id.opp_secondary);
+        oppTertiary = (ImageView) findViewById(R.id.opp_tertiary);
+
+        userUsername = (TextView) findViewById(R.id.user_username);
+        userLevel = (TextView) findViewById(R.id.user_level);
+        userHealthbar = (TextView) findViewById(R.id.user_healthbar);
+        userEnergybar = (TextView) findViewById(R.id.user_energybar);
+        userPrimary = (ImageView) findViewById(R.id.user_primary);
+        userSecondary = (ImageView) findViewById(R.id.user_secondary);
+        userTertiary = (ImageView) findViewById(R.id.user_tertiary);
 
         requestQueue = Volley.newRequestQueue(this);
 
@@ -66,21 +106,6 @@ public class Battle extends AppCompatActivity {
     }
 
     private void setupBattleScreen() {
-        TextView oppUsername = (TextView) findViewById(R.id.opponent_username);
-        TextView oppLevel = (TextView) findViewById(R.id.opponent_level);
-        TextView oppHealthbar = (TextView) findViewById(R.id.opponent_healthbar);
-        ImageView oppPrimary = (ImageView) findViewById(R.id.opp_primary);
-        ImageView oppSecondary = (ImageView) findViewById(R.id.opp_secondary);
-        ImageView oppTertiary = (ImageView) findViewById(R.id.opp_tertiary);
-
-        TextView userUsername = (TextView) findViewById(R.id.user_username);
-        TextView userLevel = (TextView) findViewById(R.id.user_level);
-        TextView userHealthbar = (TextView) findViewById(R.id.user_healthbar);
-        TextView userEnergybar = (TextView) findViewById(R.id.user_energybar);
-        ImageView userPrimary = (ImageView) findViewById(R.id.user_primary);
-        ImageView userSecondary = (ImageView) findViewById(R.id.user_secondary);
-        ImageView userTertiary = (ImageView) findViewById(R.id.user_tertiary);
-
         try {
             oppUsername.setText(battleData.getString("opp_id"));
             oppLevel.setText("Level " + battleData.getInt("opp_level"));
@@ -116,6 +141,76 @@ public class Battle extends AppCompatActivity {
             utils.setSpaceshipColors(userPrimary, "primary", userData.getString("color_1"));
             utils.setSpaceshipColors(userSecondary, "secondary", userData.getString("color_2"));
             utils.setSpaceshipColors(userTertiary, "tertiary", userData.getString("color_3"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fireLaser(View view) {
+        try {
+            String[] keys = {"user_id", "opp_id"};
+            String[] values = {userData.getString("user_id"), battleData.getString("opp_id")};
+            JSONObject data = utils.createJSONObj(keys, values);
+
+            String url = utils.URL + "fire_laser";
+            JsonObjectRequest req = new JsonObjectRequest(url, data,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                ImageView laser1 = findViewById(R.id.img_laser_1);
+                                ImageView laser2 = findViewById(R.id.img_laser_2);
+
+                                ConstraintLayout container = (ConstraintLayout) findViewById(R.id.battle_scene);
+                                TransitionSet set = new TransitionSet();
+                                Transition energyTransition = new ChangeBounds();
+                                Transition laserTransition1 = new ChangeBounds();
+                                Transition laserTransition2 = new ChangeBounds();
+                                Transition healthTransition = new ChangeBounds();
+                                energyTransition.addTarget(userEnergybar);
+                                laserTransition1.addTarget(laser1);
+                                laserTransition2.addTarget(laser2);
+                                healthTransition.setStartDelay(500).addTarget(oppHealthbar);
+
+                                set.addTransition(energyTransition).addTransition(healthTransition);
+
+                                TransitionManager.beginDelayedTransition(container, set);
+
+                                // TODO: animate lasers, energy decrease and lasers at same time, delay and then change health bar
+
+                                int remainingHp = response.getInt("remaining_hp");
+                                ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) oppHealthbar.getLayoutParams();
+                                params.width = (int) remainingHp * getResources().getDimensionPixelSize(R.dimen.bar_size) / 100;
+                                if(remainingHp == 0)
+                                    params.width = getResources().getDimensionPixelSize(R.dimen.empty_bar_size);
+                                oppHealthbar.setLayoutParams(params);
+
+                                int remainingEnergy = response.getInt("remaining_energy");
+                                params = (ConstraintLayout.LayoutParams) userEnergybar.getLayoutParams();
+                                params.width = (int) remainingEnergy * getResources().getDimensionPixelSize(R.dimen.bar_size) / 100;
+                                if(remainingEnergy == 0)
+                                    params.width = getResources().getDimensionPixelSize(R.dimen.empty_bar_size);
+                                else if(remainingEnergy > 100)
+                                    params.width = getResources().getDimensionPixelSize(R.dimen.bar_size);
+                                userEnergybar.setLayoutParams(params);
+
+                                if(response.getBoolean("won_battle")) {
+                                    Button btnFire = (Button) findViewById(R.id.btn_fire);
+                                    btnFire.setText("Won Battle!");
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //Handle Errors here
+                        }
+                    });
+
+            requestQueue.add(req);
         } catch (JSONException e) {
             e.printStackTrace();
         }
