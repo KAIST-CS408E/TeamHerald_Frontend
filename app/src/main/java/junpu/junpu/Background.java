9f98323b1d5eccfa039f5b7e86279e7bebe37865
kebobojames <friendofukf@gmail.com>
2018-11-19 22:19:38 +0900

    adding detection (weather, inter, wrong lane)

--- app/src/main/java/junpu/junpu/Background.java
+++ app/src/main/java/junpu/junpu/Background.java
@@ -1,5 +1,6 @@
 package junpu.junpu;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Notification;
@@ -10,6 +11,8 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -22,9 +25,11 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
@@ -32,11 +37,16 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.state.Weather;
+import com.google.android.gms.awareness.snapshot.WeatherResponse;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -45,7 +55,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 public class Background extends Service implements LocationListener, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, SensorEventListener {
@@ -70,13 +82,38 @@ public class Background extends Service implements LocationListener, GoogleApiCl
     public enum STATE{
         NOT_BIKING,BIKING
     }
-    public STATE state = STATE.NOT_BIKING;
 
     //General stats
     public float totalDistance;
 
+    // list of intersections and roads
+    List<List<String>> intersections;
+    List<List<String>> roads;
+
+    // Constants/multipliers for weather conditions
+    final int normalspeed = 7;
+    final float rainspeedmultiplier = 0.4f;
+    final float intersectionspeedmultiplier = 0.6f;
+
+    // Keeping track of previous locations
+    double prevlat, prevlong;
+
+    // Weather stuff
+    int currentday = 0;
+    int[] condition;
+    Boolean todayRain;
+
+    // state checking
+    public STATE state = STATE.NOT_BIKING;
+    public int atIntersection = 0;
+    public int raining = 0;
+
     //violations
     public boolean phoneViolation = false;
+    public boolean speeding = false;
+    public boolean intersectionSpeeding = false;
+    public boolean rainBiking = false;
+    public boolean wrongLane = false;
     public long startTime;
 
 
@@ -84,6 +121,33 @@ public class Background extends Service implements LocationListener, GoogleApiCl
     public void onCreate(){
         super.onCreate();
 //        Log.d("TAG", "Service: onCreate");
+
+        // create the lists
+        intersections = new ArrayList<List<String>>();
+
+        for(String line : Constants.intersections_string)
+        {
+            String[] linePieces = line.split(",");
+            List<String> csvPieces = new ArrayList<String>(linePieces.length);
+            for(String piece : linePieces)
+            {
+                csvPieces.add(piece);
+            }
+            intersections.add(csvPieces);
+        }
+
+        roads = new ArrayList<List<String>>();
+
+        for(String line : Constants.roads_string)
+        {
+            String[] linePieces = line.split(",");
+            List<String> csvPieces = new ArrayList<String>(linePieces.length);
+            for(String piece : linePieces)
+            {
+                csvPieces.add(piece);
+            }
+            roads.add(csvPieces);
+        }
 
         //notifications
         final NotificationCompat.Builder builder = getNotificationBuilder(this,
@@ -136,23 +200,167 @@ public class Background extends Service implements LocationListener, GoogleApiCl
         else{
             Log.e("TAG", "Valid location update");
 
-        if (lStart == null) {
-            lStart = location;
-            lEnd = location;
-        } else {
-            lEnd = location;
-            float temp = location.distanceTo(mCurrentLocation);
-            totalDistance += temp;
-        }
-        mCurrentLocation = location;
+            if (lStart == null) {
+                lStart = location;
+                lEnd = location;
+            } else {
+                lEnd = location;
+                float temp = location.distanceTo(mCurrentLocation);
+                totalDistance += temp;
+            }
+            mCurrentLocation = location;
 
-        Log.e("TAG", "Total distance: "+String.valueOf(totalDistance));
+            Log.e("TAG", "Total distance: "+String.valueOf(totalDistance));
 
-        //Calling the method below updates the  live values of distance and speed to the TextViews.
-        //calculating the speed with getSpeed method it returns speed in m/s so we are converting it into kmph
-        float speed = location.getSpeed() * 18 / 5;
+            if (state == STATE.BIKING) {
+                Log.e("TAG","I'M BIKING!");
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                float speed = location.getSpeed();
 
-        Log.e("TAG", "Speed" + Float.toString(speed));
+                int idx;
+                double right1lat, right1long, right2lat, right2long, left1lat, left1long, left2lat, left2long;
+
+                // linear search for wrong lane
+                for (idx = 1; idx<roads.size(); idx++) {
+                    String whichSideOfRoad = null;
+                    String whichWayDriving = null;
+                    String orientation = roads.get(idx).get(9);
+                    boolean inRoad = false;
+                    left1lat = Double.parseDouble(roads.get(idx).get(1));
+                    left1long = Double.parseDouble(roads.get(idx).get(2));
+                    left2lat = Double.parseDouble(roads.get(idx).get(3));
+                    left2long = Double.parseDouble(roads.get(idx).get(4));
+                    right1lat = Double.parseDouble(roads.get(idx).get(5));
+                    right1long = Double.parseDouble(roads.get(idx).get(6));
+                    right2lat = Double.parseDouble(roads.get(idx).get(7));
+                    right2long = Double.parseDouble(roads.get(idx).get(8));
+
+                    if (orientation == "V") {
+                        if (left1long - left2long == 0) {
+                            if (latitude <= left1lat && latitude >= left2lat && longitude >= left1long && longitude <= right1long) {
+                                // we inside a road bois
+                                Log.e("TAG","ON A ROAD!");
+                                inRoad = true;
+                                if (prevlat > 0.0d) {
+                                    if (latitude - prevlat > 0) {
+                                        whichWayDriving = "R";
+                                    } else {
+                                        whichWayDriving = "L";
+                                    }
+                                }
+
+                            }
+                        } else {
+                            double slopeL = (left1long - left2long) / (left1lat - left2lat);
+                            double slopeR = (right1long - right2long) / (right1lat - right2lat);
+                            if (latitude <= left1lat && latitude >= left2lat && longitude >= slopeL*(latitude-left2lat) + left2long && longitude <= slopeR*(latitude-right2lat) + right2long) {
+                                // we inside a road bois
+                                Log.e("TAG","ON A ROAD!");
+                                inRoad = true;
+                                if (prevlat > 0.0d) {
+                                    if (latitude - prevlat > 0) {
+                                        whichWayDriving = "R";
+                                    } else {
+                                        whichWayDriving = "L";
+                                    }
+                                }
+
+                            }
+                        }
+                    } else {
+                        if (left1lat - left2lat == 0) {
+                            if (latitude <= left1lat && latitude >= right2lat && longitude <= left1long && longitude >= left2long) {
+                                // we inside a road bois
+                                Log.e("TAG","ON A ROAD!");
+                                inRoad = true;
+                                if (prevlong > 0.0d) {
+                                    if (longitude - prevlong > 0) {
+                                        whichWayDriving = "R";
+                                    } else {
+                                        whichWayDriving = "L";
+                                    }
+                                }
+                            }
+                        } else {
+                            double slopeL = (left1lat - left2lat) / (left1long - left2long);
+                            double slopeR = (right1lat - right2lat) / (right1long - right2long);
+                            if (longitude <= left1long && longitude >= left2long && latitude >= slopeR*(longitude-right2long) + right2lat && latitude <= slopeL*(longitude-left2long) + left2lat) {
+                                // we inside a road bois
+                                Log.e("TAG","ON A ROAD!");
+                                inRoad = true;
+                                if (prevlong > 0.0d) {
+                                    if (longitude - prevlong > 0) {
+                                        whichWayDriving = "R";
+                                    } else {
+                                        whichWayDriving = "L";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (inRoad) {
+                        double disWithRight = Math.abs((right2long - right1long) * latitude - (right2lat - right1lat) * longitude + right2lat * right1long - right2long * right1lat)
+                                / Math.sqrt(Math.pow(right2long - right1long, 2) + Math.pow(right2lat - right1lat, 2));
+                        double disWithLeft = Math.abs((left2long - left1long) * latitude - (left2lat - left1lat) * longitude + left2lat * left1long - left2long * left1lat)
+                                / Math.sqrt(Math.pow(left2long - left1long, 2) + Math.pow(left2lat - left1lat, 2));
+
+                        if (disWithRight < disWithLeft) {
+                            // Closer to RIGHT
+                            whichSideOfRoad = "R";
+                        } else {
+                            // Closer to LEFT
+                            whichSideOfRoad = "L";
+                        }
+                        Log.e("TAG","CHECKING WRONG LANE?!?!?!");
+                        if (whichWayDriving != null && !whichSideOfRoad.equals(whichWayDriving)) {
+                            wrongLane = true;
+                            Log.e("TAG","WRONG LANE MOTHER FUCKER");
+                        }
+                        break;
+                    }
+                }
+
+                prevlat = latitude;
+                prevlong = longitude;
+
+                //linear search for intersection
+                for (idx = 1; idx<intersections.size(); idx++){
+                    double templat = Double.parseDouble(intersections.get(idx).get(1));
+                    if (latitude < templat - 0.0001) {
+                        atIntersection = 0;
+                        break;
+                    }
+                    if (latitude <= templat + 0.0001 && latitude >= templat - 0.0001) {
+                        double templong = Double.parseDouble(intersections.get(idx).get(2));
+                        if (longitude <= templong + 0.0001 && longitude >= templong - 0.0001) {
+                            // we in the intersection bois.
+                            atIntersection = 1;
+                            Log.e("TAG","ON AN INTERSECTION");
+                            break;
+                        }
+                    }
+
+                }
+                if (idx == intersections.size()) {
+                    atIntersection = 0;
+                }
+
+                // speed checking
+                float speedLimit = normalspeed*(rainspeedmultiplier+(1-rainspeedmultiplier)*(1-raining))*(intersectionspeedmultiplier+(1-intersectionspeedmultiplier)*(1-atIntersection));
+                Log.e("TAG","AM I HERE?!?!?");
+                if (speedLimit < speed) {
+                    Log.e("TAG","I AM SPEEDING!");
+                    speeding = true;
+                    if (atIntersection == 1) {
+                        intersectionSpeeding = true;
+                    }
+                    if (raining == 1) {
+                        rainBiking = true;
+                    }
+                }
+            }
         }
     }
 
@@ -173,8 +381,15 @@ public class Background extends Service implements LocationListener, GoogleApiCl
 
         //general stats
         totalDistance = 0;
-
         phoneViolation = false;
+        speeding = false;
+        intersectionSpeeding = false;
+        rainBiking = false;
+        wrongLane = false;
+        atIntersection = 0;
+        raining = 0;
+        prevlat = 0;
+        prevlong = 0;
         startTime = Calendar.getInstance().getTimeInMillis();
     }
 
@@ -203,6 +418,49 @@ public class Background extends Service implements LocationListener, GoogleApiCl
 
 
     private void handleActivity(Intent intent){
+        Calendar cal2;
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Background.this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        cal2 = Calendar.getInstance();
+        int tempday = cal2.get(Calendar.DAY_OF_MONTH);
+        if (currentday == 0) {
+            currentday = tempday;
+        } else if (currentday != tempday){
+            if (tempday - currentday == 1) {
+                currentday = tempday;
+                if (todayRain != null) {
+                    editor.putBoolean("rain", todayRain);
+                    todayRain = null;
+                    editor.apply();
+                }
+            } else {
+                editor.remove("rain");
+                editor.apply();
+                todayRain = null;
+            }
+        }
+
+        getWeatherSnapshot();
+
+        if (condition != null) {
+            if (Arrays.asList(condition).contains(5) || Arrays.asList(condition).contains(6) || Arrays.asList(condition).contains(7) || Arrays.asList(condition).contains(8)) {
+                todayRain = true;
+            } else if (todayRain == null) {
+                todayRain = false;
+            }
+        }
+
+        boolean ytdrain = sharedPreferences.getBoolean("rain", false);
+        if (todayRain != null) {
+            if (ytdrain || todayRain) {
+                raining = 1;
+            }
+        } else {
+            if (ytdrain) {
+                raining = 1;
+            }
+        }
+
 //        Log.d("TAG", "Service: handleActivity");
         ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
 
@@ -436,5 +694,26 @@ public class Background extends Service implements LocationListener, GoogleApiCl
                     haveConnectedMobile = true;
         }
         return haveConnectedWifi || haveConnectedMobile;
+    }
+
+    private void getWeatherSnapshot() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Awareness.getSnapshotClient(this).getWeather()
+                    .addOnSuccessListener(new OnSuccessListener<WeatherResponse>() {
+                        @Override
+                        public void onSuccess(WeatherResponse weatherResponse) {
+                            Weather weather = weatherResponse.getWeather();
+                            condition = weather.getConditions();
+                            //Log.e("TAG","weather" + weather);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e("TAG","Could not get weather: " + e);
+                        }
+                    });
+        }
     }
 }
