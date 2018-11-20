21abea30de68bb8512180afcee06757ee711f6c7
tsook <kimts0830@gmail.com>
2018-11-20 22:54:54 +0900

    Implement achievements activity display

--- app/src/main/java/junpu/junpu/utils.java
+++ app/src/main/java/junpu/junpu/utils.java
@@ -4,8 +4,34 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class utils {
     public static String URL = "https://junpu.herokuapp.com/";
+
+    public static String[][] achievementsList = {
+            {"Victory!", "Get your first win"},
+            {"Clutch", "Barely beat your opponent"},
+            {"Domination", "Kill the same opponent 3 times"},
+            {"Revenge", "Kill an opponent that previously killed you"},
+            {"Smooth Riding", "Have a perfectly safe biking session"},
+            {"Perfection", "Kill an opponent with only safe sessions"},
+            {"Destroyer", "Get 100 wins"},
+            {"Hunter", "Kill 5 different opponents"},
+            {"Climbing the Ladder", "Get 10 wins"},
+            {"Space Pirate", "Get 50 wins"},
+            {"Top of the World", "Get 25 wins"},
+            {"Leveling Up", "Get to Level 15"},
+            {"Hobbyist", "Log 500 biking sessions"},
+            {"Better Safe than Sorry", "Log 250 biking safe sessions"},
+            {"Safe Landing", "Get 50 consecutive safe sessions"},
+            {"Getting There", "Get 10 consecutive safe sessions"},
+            {"Space Shooter", "Fire 500 times"},
+            {"Easy Peasy", "Kill an opponent with more than half your health remaining"},
+            {"Masterful", "Have a win/loss ratio above 60% with more than 50 battles"},
+            {"Augmented Intelligence", "Get 10 kills in a row"}
+    };
+
 
     public static JSONObject createJSONObj(String keys[], String values[]) {
         JSONObject result = new JSONObject();
